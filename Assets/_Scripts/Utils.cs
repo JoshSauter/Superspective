9fcf40463d46c3baef861365712f04e70c6dc094
JoshSauter <jsauter@salesforce.com>
2018-01-30 19:09:06 -0800

    RisingBridge built and in debug mode

---
+++ Assets/_Scripts/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EpitaphUtils {
+	public class Utils {
+		public static T[] GetComponentsInChildrenOnly<T>(Transform parent) where T : Component {
+			T[] all = parent.GetComponentsInChildren<T>();
+			T[] children = new T[parent.childCount];
+			int index = 0;
+			foreach (T each in all) {
+				if (each.transform != parent) {
+					children[index] = each;
+					index++;
+				}
+			}
+
+			return children;
+		}
+	}
+}
