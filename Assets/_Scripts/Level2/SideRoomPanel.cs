05e3831a4f2b7637c3e8c23252b3351ddc8ed6f9
JoshSauter <jsauter@salesforce.com>
2018-02-07 00:03:34 -0800

    Unity keeps crashing but started the laser effect

--- Assets/_Scripts/Level2/SideRoomPanel.cs
+++ Assets/_Scripts/Level2/SideRoomPanel.cs
@@ -6,14 +6,23 @@ public class SideRoomPanel : MonoBehaviour {
 	Color gemColor;
 	Button gemButton;
 
+	public Renderer roomRenderer;
 	Renderer thisRenderer;
 	public float colorLerpTime = 1.75f;
-	MaterialPropertyBlock propBlock;
+	public float roomEmissionLevel = 0.2f;
+	public Color startingRoomColor = new Color(0.1f, 0.1f, 0.1f);
+	MaterialPropertyBlock panelPropBlock;
+	MaterialPropertyBlock roomPropBlock;
 
 	// Use this for initialization
 	void Start () {
-		propBlock = new MaterialPropertyBlock();
+		panelPropBlock = new MaterialPropertyBlock();
+		roomPropBlock = new MaterialPropertyBlock();
 		thisRenderer = GetComponent<Renderer>();
+
+		roomRenderer.GetPropertyBlock(roomPropBlock);
+		roomPropBlock.SetColor("_Color", startingRoomColor);
+		roomRenderer.SetPropertyBlock(roomPropBlock);
 
 		gemButton = GetComponentInChildren<Button>();
 		gemColor = gemButton.GetComponent<MeshRenderer>().material.color;
@@ -21,14 +30,15 @@ public class SideRoomPanel : MonoBehaviour {
 	}
 
 	void PanelActivate(Button b) {
-		StartCoroutine(ColorLerp());
+		StartCoroutine(PanelColorLerp());
+		StartCoroutine(RoomColorLerp());
 
 		b.GetComponent<ObjectHover>().hoveringPaused = true;
 		b.GetComponent<RotateObject>().enabled = false;
 	}
 
-	IEnumerator ColorLerp() {
-		thisRenderer.GetPropertyBlock(propBlock);
+	IEnumerator PanelColorLerp() {
+		thisRenderer.GetPropertyBlock(panelPropBlock);
 		Color startColor = thisRenderer.material.color;
 
 		float timeElapsed = 0;
@@ -36,12 +46,35 @@ public class SideRoomPanel : MonoBehaviour {
 			timeElapsed += Time.deltaTime;
 			float t = timeElapsed / colorLerpTime;
 
-			propBlock.SetColor("_Color", Color.Lerp(startColor, gemColor, t));
-			thisRenderer.SetPropertyBlock(propBlock);
+			Color curColor = Color.Lerp(startColor, gemColor, t);
+			panelPropBlock.SetColor("_Color", curColor);
+			thisRenderer.SetPropertyBlock(panelPropBlock);
 
 			yield return null;
 		}
-		propBlock.SetColor("_Color", gemColor);
-		thisRenderer.SetPropertyBlock(propBlock);
+		panelPropBlock.SetColor("_Color", gemColor);
+		thisRenderer.SetPropertyBlock(panelPropBlock);
+	}
+
+	IEnumerator RoomColorLerp() {
+		roomRenderer.GetPropertyBlock(roomPropBlock);
+		Color startColor = roomRenderer.material.color;
+
+		yield return new WaitForSeconds(colorLerpTime/2f);
+
+		float timeElapsed = 0;
+		while (timeElapsed < colorLerpTime) {
+			timeElapsed += Time.deltaTime;
+			float t = timeElapsed / colorLerpTime;
+
+			Color curColor = Color.Lerp(startColor, gemColor, t);
+			roomPropBlock.SetColor("_Color", curColor);
+			roomPropBlock.SetColor("_EmissionColor", curColor * roomEmissionLevel);
+			roomRenderer.SetPropertyBlock(roomPropBlock);
+
+			yield return null;
+		}
+		roomPropBlock.SetColor("_Color", gemColor);
+		roomRenderer.SetPropertyBlock(panelPropBlock);
 	}
 }
