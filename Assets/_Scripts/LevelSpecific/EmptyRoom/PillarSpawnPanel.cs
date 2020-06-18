9379a9ac0dd36b35b16cd53434812eda4657a323
JoshSauter <jsauter@salesforce.com>
2020-06-18 00:23:07 -0700

    One more small reference upgrade

--- Assets/_Scripts/LevelSpecific/EmptyRoom/PillarSpawnPanel.cs
+++ Assets/_Scripts/LevelSpecific/EmptyRoom/PillarSpawnPanel.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MagicTriggerMechanics;
 
 public class PillarSpawnPanel : Panel {
 	public GameObject pillarBeforeActive;
 	public DimensionPillar pillar;
-	public ForceActivePillarToggle pillarActiveTrigger;
+	public MagicTriggerNew pillarActiveTrigger;
 
 	override protected void Start() {
 		base.Start();
