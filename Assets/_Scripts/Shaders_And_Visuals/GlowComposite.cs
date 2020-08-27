6cae5287555aa23f462d5d02ff7a4cb94f2793b1
JoshSauter <jsauter@salesforce.com>
2020-08-27 12:58:57 -0700

    Possible fix for interactable objects on other machines

--- Assets/_Scripts/Shaders_And_Visuals/GlowComposite.cs
+++ Assets/_Scripts/Shaders_And_Visuals/GlowComposite.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 
-[ExecuteInEditMode]
 [RequireComponent(typeof(Camera))]
 public class GlowComposite : MonoBehaviour {
 	[Range(0, 10)]
 	public float Intensity = 2;
 
+	public RenderTexture test;
 	private Material _compositeMat;
 
 	void OnEnable() {
 		_compositeMat = new Material(Shader.Find("Hidden/GlowComposite"));
+		test = new RenderTexture(EpitaphScreen.currentWidth, EpitaphScreen.currentHeight, 24);
 	}
 
 	void OnRenderImage(RenderTexture src, RenderTexture dst) {
