e7f35751447c3f1b1e89c1eb9137b86cbb6994b3
JoshSauter <jsauter@salesforce.com>
2021-02-26 22:38:48 -0800

    Added a bit of spice to WhiteRoom1 hallway, time to rename everything

---
+++ Assets/_Scripts/LevelSpecific/WhiteRoom/Trapdoor.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using EpitaphUtils;
+using PowerTrailMechanics;
+using UnityEngine;
+
+namespace LevelSpecific.WhiteRoom {
+    public class Trapdoor : MonoBehaviour {
+        public PowerTrail powerTrailToReactTo;
+        
+        public enum State {
+            Closed,
+            Opening,
+            Open,
+            Closing
+        }
+
+        State _state;
+
+        State state {
+            get => _state;
+            set {
+                if (_state == value) {
+                    return;
+                }
+
+                _state = value;
+                timeSinceStateChanged = 0f;
+            }
+        }
+        float timeSinceStateChanged = 0f;
+        const float openCloseTime = 1.25f;
+
+        readonly Vector3 closedScale = Vector3.one;
+        readonly Vector3 openScale = new Vector3(0.15f, 0.15f, 1f);
+
+        Transform[] trapdoorPieces;
+
+        void Awake() {
+            trapdoorPieces = transform.GetChildren().Take(2).ToArray();
+        }
+
+        void Update() {
+            UpdateState();
+            
+            timeSinceStateChanged += Time.deltaTime;
+            float t = timeSinceStateChanged / openCloseTime;
+            
+            switch (state) {
+                case State.Closed:
+                    foreach (var piece in trapdoorPieces) {
+                        piece.localScale = closedScale;
+                    }
+                    break;
+                case State.Opening:
+                    foreach (var piece in trapdoorPieces) {
+                        piece.localScale = Vector3.Lerp(closedScale, openScale, Easing.EaseInOut(t));
+                    }
+                    break;
+                case State.Open:
+                    foreach (var piece in trapdoorPieces) {
+                        piece.localScale = openScale;
+                    }
+                    break;
+                case State.Closing:
+                    foreach (var piece in trapdoorPieces) {
+                        piece.localScale = Vector3.Lerp(openScale, closedScale, Easing.EaseInOut(t));
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        void UpdateState() {
+            switch (state) {
+                case State.Closed:
+                    if (powerTrailToReactTo.fullyPowered) {
+                        state = State.Opening;
+                    }
+                    break;
+                case State.Opening:
+                    if (!powerTrailToReactTo.fullyPowered) {
+                        float cachedTimeSinceStateChange = timeSinceStateChanged;
+                        state = State.Closing;
+                        // Don't start from 0 if opening or closing was interrupted
+                        timeSinceStateChanged = 1 - cachedTimeSinceStateChange;
+                    }
+                    else if (timeSinceStateChanged > openCloseTime) {
+                        state = State.Open;
+                    }
+                    break;
+                case State.Open:
+                    if (!powerTrailToReactTo.fullyPowered) {
+                        state = State.Closing;
+                    }
+                    break;
+                case State.Closing:
+                    if (powerTrailToReactTo.fullyPowered) {
+                        float cachedTimeSinceStateChange = timeSinceStateChanged;
+                        state = State.Opening;
+                        // Don't start from 0 if opening or closing was interrupted
+                        timeSinceStateChanged = 1 - cachedTimeSinceStateChange;
+                    }
+                    else if (timeSinceStateChanged > openCloseTime) {
+                        state = State.Closed;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
