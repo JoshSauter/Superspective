8abe523ff0f10d989b0db1e8142c05e1c927c5bf
JoshSauter <sauterj@umich.edu>
2024-10-03 23:50:33 -0400

    Fix ProBuilder meshes
    ProBuilder meshes like to get messed up for no reason whatsoever and break builds. I wrote a script to identify the broken ProBuilder meshes to speed up the manual recovery process, then used that tool to fix the build

---
+++ Assets/Editor/IdentifyBrokenProBuilderMeshesTool.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.ProBuilder.MeshOperations;
+
+namespace Editor {
+    public class IdentifyBrokenProBuilderMeshesTool : EditorWindow {
+        
+        [MenuItem("Tools/ProBuilder/Repair/Identify Broken ProBuilder Meshes")]
+        public static void FindBrokenPBMeshesInSelection() {
+            int brokenCount = 0;
+            foreach (var go in Selection.gameObjects) {
+                ProBuilderMesh pbMesh = go.GetComponent<ProBuilderMesh>();
+                if (pbMesh) {
+                    try {
+                        MeshValidation.RemoveDegenerateTriangles(pbMesh);
+                    }
+                    catch {
+                        brokenCount++;
+                        Debug.LogError($"Found broken mesh on {go.name}", go);
+                    }
+                }
+            }
+            
+            if (brokenCount == 0) {
+                Debug.Log("<color=green>No broken ProBuilderMeshes found in selection!</color>");
+            }
+            else {
+                Debug.LogWarning($"<color=red>Found {brokenCount} broken ProBuilderMeshes in selection</color>");
+            }
+        }
+    }
+}
